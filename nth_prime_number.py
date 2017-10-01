d3192eae8c6a4ed791809a296ff88250a7813c30
Hasnan <hasnainy370@gmail.com>
2017-10-01 11:58:46 -0700

    Update nth_prime_number.py

--- nth_prime_number.py
+++ nth_prime_number.py
@@ -1,5 +1,10 @@
 import math
 def nthPrimeNumber(Range):
+    """
+    This function returns an nth prime number.
+    Range is an integer value which is passed
+    by the user of this function.
+    """
     primeNumbers=[2]
     b=3
     while len(primeNumbers)<Range:
